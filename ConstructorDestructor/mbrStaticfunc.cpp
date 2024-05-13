82b7e5d9a4310ddcdef0d7de6c47456b984a8a47
verss21 <arjunafrizal01@gmail.com>
2024-05-13 10:52:57 +0700

    static int nim private, int id dan string nama public

--- ConstructorDestructor/mbrStaticfunc.cpp
+++ ConstructorDestructor/mbrStaticfunc.cpp
@@ -4,3 +4,14 @@ using namespace std;
 
 class mahasiswa
 {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	static void setNim(int pNim) { nim = pNim; }
+};
