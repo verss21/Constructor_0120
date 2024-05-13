12fb4938f006a96c54495483124b460181de1ee1
verss21 <arjunafrizal01@gmail.com>
2024-05-13 11:10:49 +0700

    set nama

--- ConstructorDestructor/mbrStaticfunc.cpp
+++ ConstructorDestructor/mbrStaticfunc.cpp
@@ -5,9 +5,9 @@ using namespace std;
 class mahasiswa
 {
 private:
-	static int nim;
+	static long int nim;
 public:
-	int id;
+	long int id;
 	string nama;
 
 	void setID();
@@ -23,9 +23,32 @@ public:
 	}
 };	
 
-int mahasiswa::nim = 20230140120;
+long int mahasiswa::nim = 20230140120;
 
 void mahasiswa::setID()
 {
 	id = ++nim;
+}
+
+void mahasiswa::printAll()
+{
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main()
+{
+	mahasiswa mhs1("Raehan Arjun");
+	mahasiswa mhs2("Imron");
+	mahasiswa::setNim(9);// mengakses nim melalui static member function "setNim"
+	mahasiswa mhs3("Bagus");
+	mahasiswa mhs4("Rafi");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();;
+
+	cout << "akses dari luar object = " << mahasiswa::getNim() << endl;
 }
